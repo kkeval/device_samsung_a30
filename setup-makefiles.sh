064b4c3d51c8a8f3bba2d4e20465e25764d880d5
Keval Panchal <34764579+kkeval@users.noreply.github.com>
2020-09-19 20:18:22 +0530

    RR

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -7,9 +7,9 @@ DEVICE=a30
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+RR_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$RR_ROOT"/vendor/RR/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -17,7 +17,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$RR_ROOT"
 
 # Copyright headers and guards
 write_headers
